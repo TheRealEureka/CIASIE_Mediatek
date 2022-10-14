503cbfb9826189e61fb7f93a01db5dcf463746aa
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-10-14 17:11:15 +0200

    function add img to the medias

--- js/createmedia.js
+++ js/createmedia.js
@@ -11,47 +11,45 @@ let title
 let year
 let cover
 
-document.getElementById("create").addEventListener("click", function() {
-type = document.getElementById("med").value;
-switch (type) {
-case "Boo":
-    title = prompt("Enter the title of the book");
-    let author = prompt("Enter the author of the book");
-    year = prompt("Enter the release year of the book");
-    cover = prompt("Enter the cover of the book");
-    createdMedia = new Book(title, author, year, cover);
-    document.getElementById("medias").innerHTML += '<div class="media"><p>Book :'+createdMedia.title+createdMedia.author+createdMedia.releaseYear+'</p></div>';
-    break;
 
-case "Mov":
-    title = prompt("Enter the title of the movie");
-    year = prompt("Enter the release year of the movie");
-    cover = prompt("Enter the cover of the movie");
-    let realisator = prompt("Enter the director of the movie");
-    createdMedia = new Movie(title, realisator, year, cover);
-    document.getElementById("medias").innerHTML += '<div class="media"><p>Movie : '+createdMedia.title+createdMedia.director+createdMedia.releaseYear+'</p></div>';
-    break;
+document.getElementById("create").addEventListener("click", function () {
+    type = document.getElementById("med").value;
+    title = prompt("Enter the title");
+    cover = prompt("Enter the location of the img of the cover");
 
-case "Albm":
-    title = prompt("Enter the title of the album");
-    year = prompt("Enter the release year of the album");
-    cover = prompt("Enter the cover of the album");
-    let artist = prompt("Enter the artist of the album");
-    createdMedia = new Album(title, artist, year, cover);
-    document.getElementById("medias").innerHTML += '<div class="media"><p>Album : '+createdMedia.title+createdMedia.artist+createdMedia.releaseYear+'</p></div>';
-    break;
+    switch (type) {
+        case "Boo":
+            let author = prompt("Enter the author of the book");
+            year = prompt("Enter the release year of the book");
+            createdMedia = new Book(title, author, year, cover);
+            document.getElementById("medias").innerHTML += '<div class="media"><p>Book :' + createdMedia.title + createdMedia.author + createdMedia.releaseYear + '</p><img src="' + cover + '"></div>';
+            break;
 
-case "Gam":
-    title = prompt("Enter the title of the game");
-    year = prompt("Enter the release year of the game");
-    cover = prompt("Enter the cover of the game");
-    let studio = prompt("Enter the studio of the game");
-    createdMedia = new Game(title, studio, year, cover);
-    document.getElementById("medias").innerHTML += '<div class="media"><p>Game ! '+createdMedia.title+createdMedia._editor+createdMedia.releaseYear+'</p></div>';
-    break;
-}
+        case "Mov":
+            year = prompt("Enter the release year of the movie");
+            let realisator = prompt("Enter the director of the movie");
+            createdMedia = new Movie(title, realisator, year, cover);
+            document.getElementById("medias").innerHTML += '<div class="media"><p>Movie : ' + createdMedia.title + createdMedia.director + createdMedia.releaseYear + '</p><img src="' + cover + '"></div>';
+            break;
 
+        case "Albm":
+            year = prompt("Enter the release year of the album");
+            let artist = prompt("Enter the artist of the album");
+            createdMedia = new Album(title, artist, year, cover);
+            document.getElementById("medias").innerHTML += '<div class="media"><p>Album : ' + createdMedia.title + createdMedia.artist + createdMedia.releaseYear + '</p><img src="' + cover + '"></div>';
+            break;
 
+        case "Gam":
+            year = prompt("Enter the release year of the game");
+            let studio = prompt("Enter the studio of the game");
+            createdMedia = new Game(title, studio, year, cover);
+            document.getElementById("medias").innerHTML += '<div class="media"><p>Game ! ' + createdMedia.title + createdMedia._editor + createdMedia.releaseYear + '</p><img src="' + cover + '"></div>';
+            break;
+    }
 
 
 });
+
+
+
+

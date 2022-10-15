2006d574fc877a84d7b1d9865e367c67c3e65b91
Eurêka <therealeureka@protonmail.com>
2022-10-15 11:31:11 +0200

    Popup form

--- js/main.js
+++ js/main.js
@@ -25,10 +25,35 @@ document.getElementById('open_popup').addEventListener('click', function(){
 
 document.getElementById('close_popup').addEventListener('click', function(){
     togglePopup()
+    displayStep(1);
 });
 
 
 
 function togglePopup(){
     document.getElementById('popup').classList.toggle('hidden');
+}
+
+
+
+//Popup form
+let steps = document.getElementsByClassName('step');
+
+document.getElementById('create').addEventListener('click', function() {
+    displayStep(2);
+});
+
+document.getElementById('popup_continue').addEventListener('click', function() {
+    displayStep(3);
+});
+
+document.getElementById('popup_back').addEventListener('click', function() {
+    displayStep(1);
+});
+
+function displayStep(step){
+    for(let i = 0; i < steps.length; i++){
+        steps[i].classList.add('hidden');
+    }
+    document.getElementById('popup_step' + step).classList.remove('hidden');
 }

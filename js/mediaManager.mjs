f7f6535705c12d6cbbdc6bd79697959dbc6f5109
Tristan <62724143+MaegIins@users.noreply.github.com>
2022-10-17 17:21:23 +0200

    incroyable, cela fonctionne

--- js/mediaManager.mjs
+++ js/mediaManager.mjs
@@ -13,7 +13,7 @@ export function addMedia(media) {
     save();
 }
 
-function display(){
+function display() {
     document.getElementById("cards").innerHTML = "";
     tab.forEach(function (media) {
         let med = media.html();
@@ -29,47 +29,67 @@ function display(){
     })
 }
 
-export function displayByType(type){
-
-}
-
-function edit(media){
-
-}
-function del(media){
-    let index = tab.indexOf(media);
-    if (index >= 0) {
-        tab.splice(index, 1);
+export function displayByType(type) {
+    document.getElementById("cards").innerHTML = "";
+    if (type === "all") {
         display();
-        save();
+    } else {
+        tab.forEach(function (media) {
+            console.log(media.type);
+            if (media.type === type) {
+                let med = media.html();
+                med.children[2].children[0].addEventListener("click", function () {
+                    edit(media)
+                    display();
+                });
+                med.children[2].children[1].addEventListener("click", function () {
+                    del(media);
+                    display();
+                });
+                document.getElementById("cards").appendChild(med);
+            }
+
+        })
     }
 }
 
+    function edit(media) {
+
+    }
+
+    function del(media) {
+        let index = tab.indexOf(media);
+        if (index >= 0) {
+            tab.splice(index, 1);
+            display();
+            save();
+        }
+    }
 
 
 //Save the list into local storage
-function save(){
-    localStorage.setItem("media", JSON.stringify(tab));
-}
+    function save() {
+        localStorage.setItem("media", JSON.stringify(tab));
+    }
 
 //Get the list from local storage
-function get() {
-    let data = localStorage.getItem("media");
-    if (data !== null) {
-        let str = JSON.parse(data);
-        str.forEach(function (media) {
-            if (media._author !== undefined) {
-                tab.push(new Book(media._title, media._author, media._release, media._cover));
-            }
-            if (media._director !== undefined) {
-                tab.push(new Movie(media._title, media._director, media._release, media._cover));
-            }
-            if (media._artist !== undefined) {
-                tab.push(new Album(media._title, media._artist, media._release, media._cover));
-            }
-            if (media._editor !== undefined) {
-                tab.push(new Game(media._title, media._editor, media._release, media._cover));
-            }
-        });
+    function get() {
+        let data = localStorage.getItem("media");
+        if (data !== null) {
+            let str = JSON.parse(data);
+            str.forEach(function (media) {
+                if (media._author !== undefined) {
+                    tab.push(new Book(media._title, media._author, media._release, media._cover));
+                }
+                if (media._director !== undefined) {
+                    tab.push(new Movie(media._title, media._director, media._release, media._cover));
+                }
+                if (media._artist !== undefined) {
+                    tab.push(new Album(media._title, media._artist, media._release, media._cover));
+                }
+                if (media._editor !== undefined) {
+                    tab.push(new Game(media._title, media._editor, media._release, media._cover));
+                }
+            });
+        }
     }
-}
